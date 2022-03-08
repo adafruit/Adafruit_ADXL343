d9e25f1fdd7be3c445b7dbf6aaa75b90ee9dc968
Doxygen CI <ci-arduino@invalid>
2022-03-08 00:38:26 +0000

    Deploy docs to GitHub Pages from commit ada5ab2e61
    Commit: ada5ab2e611b163507716b74f0e213bf2dbda9df
    GitHub Actions run: 1948754478

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -7,6 +7,7 @@ var searchData=
   ['getsensor',['getSensor',['../class_adafruit___a_d_x_l343.html#aac54a36d585fa7cbe42a88290e37c500',1,'Adafruit_ADXL343']]],
   ['gettrimoffsets',['getTrimOffsets',['../class_adafruit___a_d_x_l343.html#a659b2738e029e67383dd0e952e115801',1,'Adafruit_ADXL343']]],
   ['getx',['getX',['../class_adafruit___a_d_x_l343.html#acba8a90ba0532baae64acf67c9338c4a',1,'Adafruit_ADXL343']]],
+  ['getxyz',['getXYZ',['../class_adafruit___a_d_x_l343.html#a60fe2b7b5ac74dab58d513a588ecd7eb',1,'Adafruit_ADXL343']]],
   ['gety',['getY',['../class_adafruit___a_d_x_l343.html#a0247281d12263afaa21ef5cf2f0a320a',1,'Adafruit_ADXL343']]],
   ['getz',['getZ',['../class_adafruit___a_d_x_l343.html#aa98856e2a01d03d5599b72fa0a7233bd',1,'Adafruit_ADXL343']]]
 ];
